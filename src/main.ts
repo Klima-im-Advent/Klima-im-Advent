5778934a07cd45f5f7fb60c2c7d9626520b1efea
Vincent Woltmann <vincent.woltmann@hu-berlin.de>
2020-11-10 18:04:52 +0100

    Added linting

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import * as express from 'express'; 
+import * as express from "express";
 
-let app = express();
+const app = express();
 
-app.get('/', (request, response) => {
-  response.send("Es scheint zu funktionieren");
+app.get("/", (request, response) => {
+	response.send("Es scheint zu funktionieren");
 });
 
 const port = process.env.PORT || 2412;
 app.listen(port, () => {
-  console.log("Now listening on %s", port);
-})
+	console.log("Now listening on %s", port);
+});
