64e330beabedb8aa33f08c4bf42d8b6e08301e01
Vincent Woltmann <vincent.woltmann@hu-berlin.de>
2020-12-09 03:37:38 +0100

    Türchen 9

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (request, response) => {
-	response.render("index", { currentDay: 8 });
+	response.render("index", { currentDay: 9 });
 });
 app.get("/tag/:number", (request, response) => {
 	if (request.params.number === "6") {
