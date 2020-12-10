4e8680d0f79b48f287460e57d643143d26840cef
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-12-11 00:31:34 +0100

    lol why?

--- www/tag_11.ts
+++ www/tag_11.ts
@@ -7,29 +7,29 @@ const show_apple = document.getElementsByClassName("apple");
 const show_orange = document.getElementsByClassName("orange");
 
 orange.addEventListener("click", () => {
-  for (const element of has_to_switch) {
+  for (const element of has_to_switch as any) {
     const e = element as HTMLElement;
     e.style.background = "#70041F";
   }
-  for (const element of show_apple) {
+  for (const element of show_apple as any) {
     const e = element as HTMLElement;
     e.style.display = "None";
   }
-  for (const element of show_orange) {
+  for (const element of show_orange as any) {
     const e = element as HTMLElement;
     e.style.display = "Block";
   }
 })
 apple.addEventListener("click", () => {
-  for (const element of has_to_switch) {
+  for (const element of has_to_switch as any) {
     const e = element as HTMLElement;
     e.style.background = "#5B7D42";
   }
- for (const element of show_orange) {
+ for (const element of show_orange as any) {
     const e = element as HTMLElement;
     e.style.display = "None";
   }
-  for (const element of show_apple) {
+  for (const element of show_apple as any) {
     const e = element as HTMLElement;
     e.style.display = "Block";
   } 
