0f472d9c68c0142547d35fb7fc0ff5e14bac5e39
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-12-11 00:20:39 +0100

    Tag 11

---
+++ www/tag_11.ts
@@ -0,0 +1,36 @@
+const orange = document.getElementById("orange")!;
+const apple = document.getElementById("apple")!;
+
+const has_to_switch = document.getElementsByClassName("switch");
+
+const show_apple = document.getElementsByClassName("apple");
+const show_orange = document.getElementsByClassName("orange");
+
+orange.addEventListener("click", () => {
+  for (const element of has_to_switch) {
+    const e = element as HTMLElement;
+    e.style.background = "#70041F";
+  }
+  for (const element of show_apple) {
+    const e = element as HTMLElement;
+    e.style.display = "None";
+  }
+  for (const element of show_orange) {
+    const e = element as HTMLElement;
+    e.style.display = "Block";
+  }
+})
+apple.addEventListener("click", () => {
+  for (const element of has_to_switch) {
+    const e = element as HTMLElement;
+    e.style.background = "#5B7D42";
+  }
+ for (const element of show_orange) {
+    const e = element as HTMLElement;
+    e.style.display = "None";
+  }
+  for (const element of show_apple) {
+    const e = element as HTMLElement;
+    e.style.display = "Block";
+  } 
+}) 
