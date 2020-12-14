edc0fffa086200eb5e9eea424acdff5f4a547b17
Vincent Woltmann <vincent.woltmann@hu-berlin.de>
2020-12-14 02:34:25 +0100

    Tag 14 – guten Morgen

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (request, response) => {
-	response.render("index", { currentDay: 13 });
+	response.render("index", { currentDay: 14 });
 });
 app.get("/tag/:number", (request, response) => {
 	if (request.params.number === "6") {
