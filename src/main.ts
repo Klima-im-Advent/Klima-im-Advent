ca9d7e2441b8777b5af0ca6079554aa3380bdf6b
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-12-15 06:31:44 +0100

    Tag 15

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (request, response) => {
-	response.render("index", { currentDay: 14 });
+	response.render("index", { currentDay: 15 });
 });
 app.get("/tag/:number", (request, response) => {
 	if (request.params.number === "6") {
